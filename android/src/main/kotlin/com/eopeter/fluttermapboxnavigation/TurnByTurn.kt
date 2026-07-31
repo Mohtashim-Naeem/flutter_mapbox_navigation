b3d7a0a46fd2d8044d48850cf9b13b5959310358
william-dignite <150122473+William-dignite@users.noreply.github.com>
2026-07-31 00:59:17 -0700

    fixes locale error

--- android/src/main/kotlin/com/eopeter/fluttermapboxnavigation/TurnByTurn.kt
+++ android/src/main/kotlin/com/eopeter/fluttermapboxnavigation/TurnByTurn.kt
@@ -193,12 +193,11 @@ open class TurnByTurn(
             sendEvent(MapBoxEvents.ROUTE_BUILD_FAILED)
             return
         }
-        val locale = PluginUtilities.getLocaleFromCode(navigationLanguage)
         app.requestRoutes(
             routeOptions = RouteOptions
                 .builder()
                 .applyDefaultNavigationOptions(navigationMode)
-                .applyLanguageAndVoiceUnitOptions(locale)
+                .applyLanguageAndVoiceUnitOptions(context)
                 .coordinatesList(this.addedWaypoints.coordinatesList())
                 .waypointIndicesList(this.addedWaypoints.waypointsIndices())
                 .waypointNamesList(this.addedWaypoints.waypointsNames())
